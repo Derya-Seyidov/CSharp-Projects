75066308ebabccd9271efb6a500eecc3836e0f5e
Derya Seyidov <71149747+Derya-Seyidov@users.noreply.github.com>
2026-07-31 14:29:04 -0400

    Create Default.aspx.cs

---
+++ Default.aspx.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace MyFavoriteSongWebsite
+{
+    public partial class Default : System.Web.UI.Page
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
