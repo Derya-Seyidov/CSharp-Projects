7b96084e830ff367dbfb908bd06d30cdcf65f39f
Derya Seyidov <71149747+Derya-Seyidov@users.noreply.github.com>
2026-07-31 14:23:41 -0400

    Delete StudentCodeFirst/StudentContext.cs

--- StudentCodeFirst/StudentContext.cs
+++
@@ -1,7 +0,0 @@
-using System.Data.Entity;
-namespace StudentCodeFirst{
- public class StudentContext:DbContext{
-  public StudentContext():base("StudentContext"){}
-  public DbSet<Student> Students{get;set;}
- }
-}
