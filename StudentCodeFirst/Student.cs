440f47d24a640382c95502945b0d4cc32a2ac673
Derya Seyidov <71149747+Derya-Seyidov@users.noreply.github.com>
2026-07-31 14:22:58 -0400

    Delete StudentCodeFirst/Student.cs

--- StudentCodeFirst/Student.cs
+++
@@ -1,8 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-namespace StudentCodeFirst{
- public class Student{
-  [Key] public int StudentID{get;set;}
-  public string StudentName{get;set;}
-  public int Age{get;set;}
- }
-}
